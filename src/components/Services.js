9accfce24797c3c288be005c6f66bb33efd58a20
Chasecooler <chasemarq@yahoo.com>
2019-07-26 18:47:01 -0400

    item index key

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,22 +10,22 @@ export default class Services extends Component {
                        title: "Free Cocktails",
                        info:
                          'Consequat takimata at magna lorem. Lorem sanctus ipsum diam ut et at at.'
-                     }
+                     },
                      {
-                       icon: <FaCocktail />,
-                       title: "Free Cocktails",
+                       icon: <FaHiking />,
+                       title: "Endless Hiking",
                        info:
                          'Consequat takimata at magna lorem. Lorem sanctus ipsum diam ut et at at.'
-                     }
+                     },
                      {
-                       icon: <FaCocktail />,
-                       title: "Free Cocktails",
+                       icon: <FaShuttleVan />,
+                       title: "Free Shuttle",
                        info:
                          'Consequat takimata at magna lorem. Lorem sanctus ipsum diam ut et at at.'
-                     }
+                     },
                      {
-                       icon: <FaCocktail />,
-                       title: "Free Cocktails",
+                       icon: <FaBeer />,
+                       title: "Micro Brewery",
                        info:
                          'Consequat takimata at magna lorem. Lorem sanctus ipsum diam ut et at at.'
                      }
@@ -33,9 +33,21 @@ export default class Services extends Component {
                  };
                  render() {
                    return (
-                     <div>
-                       <Title title="Services" />>
-                     </div>
+                     <section className="services">
+                       <Title title="Services" />
+                       <div className="services-center">
+                       {this.state.services.map(item, index) => {
+                         return (
+                           <article key={index} className="service">
+                           <span>{item.icon}</span>
+                           <h6>{item.title}</h6>
+                           <p>{item.info}</p>
+                         </article>
+                         );
+                       })}
+
+                       </div>
+                     </section>
                    );
                  }
                }
