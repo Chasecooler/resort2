409b3bfa69a771eca875cac28c73a15f567bc9fe
Chasecooler <chasemarq@yahoo.com>
2019-07-26 17:13:01 -0400

    error at 404

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 export default function Home() {
-    return <Hero />;
+    return ( <Hero>
+        <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms Starting At $399">
+<Link to='./rooms' className="btn-primary"> Our Rooms</Link>
+        </Banner>
+    </Hero>
+    );
 }
 // return <Hero hero="defaultHero" />; ... not needed, already defined
 // const Home = () => {
