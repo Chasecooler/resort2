a77ef349b973139d6de7797a6d98ad62a1af6725
Chasecooler <chasemarq@yahoo.com>
2019-07-26 16:16:33 -0400

    rooms

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+
 
 const Rooms = () => {
-    return (
-        <div>
-            Hello from Rooms!
-        </div>
-    )
-}
+    return <Hero hero="roomsHero" />>;
+};
 
-export default Rooms
+export default Rooms;
