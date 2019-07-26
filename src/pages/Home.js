dcbc54942557ed8d51d9692488a39efbe5a8c59f
Chasecooler <chasemarq@yahoo.com>
2019-07-26 17:28:08 -0400

    save all updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import {Link} from 'react-router-dom';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
 export default function Home() {
     return ( <Hero>
         <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms Starting At $399">
