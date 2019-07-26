95a0f880b79086a23ce681131c2c3e05a199efbd
Chasecooler <chasemarq@yahoo.com>
2019-07-26 16:06:47 -0400

    hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
-const Home = () => {
-    return (
-        <div>
-            Hello from home page.
-        </div>
-    )
+import Hero from '../components/Hero';
+export default function Home() {
+    return <Hero />;
 }
+// return <Hero hero="defaultHero" />; ... not needed, already defined
+// const Home = () => {
+//     return (
+//         <div>
+//             Hello from home page.
+//         </div>
+//     )
+// }
 
-export default Home
+// export default Home
