a10d42c2149523a36bbae3e3f63eeb820277fe64
Chasecooler <chasemarq@yahoo.com>
2019-07-29 16:07:32 -0400

    update

--- src/context.js
+++ src/context.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import items from './data';
 const RoomContext = React.createContext()
 
 // export default
 class RoomProvider extends Component {
     state = {
-        greeting:'hello',
-        name:'john'
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
 
     };
+    // getData
+
+    componentDidMount(){
+        let rooms = this.formatData(items)
+    }
+    formatData(items)
+    let tempItems = items.map(item =>{
+        let id = item.sys.id
+        let images = item.fields.images.map(image => image.fields.file.url);
+
+    })
     render() {
         return (
         <RoomContext.Provider value={{...this.state}}>
